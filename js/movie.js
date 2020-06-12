8446a7ddeee133ffdbd59ff60d8bfb15614ea4d2
Agnieszka Wojtara <aganawed@me.com>
2020-06-12 18:27:32 +0200

    random number added

--- js/movie.js
+++ js/movie.js
@@ -5,22 +5,26 @@ $(document).ready(function(){
 	for (var i=0; i<allMovies.length; i++) {
 
 		let description = allMovies[i].description;
+		like = Math.floor(Math.random()* 100);
 
 		let movie = `<div class="movieBox"> 
-						<img src="${allMovies[i].image}">
+						<div class="mainImg">
+							<img src="${allMovies[i].image}">
+						</div>
 						<div class="movieInfo"> 
 								<div>
-									<h4>  ${allMovies[i].title} </h4>
+									<h3>  ${allMovies[i].title} </h3>
 									<br>
 									<p class="description"> ${allMovies[i].description}</p>
 								</div>
 								<div class="movieLikeContainer">
 									<div class="movieLikeCounter"> 
 										<p class="movieLike">Like</p>
-										<p class="likeImg">&#xf087;</p>
+										<p class="likeImg">
+											<img src="img/like.png" alt=""</p>
 									</div>
 									<div>
-										<div class="counterBox"></div>
+										<div class="counterBox">${like}</div>
 									</div> 
 								</div> 
 						</div>		
